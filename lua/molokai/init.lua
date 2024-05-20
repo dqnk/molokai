a83bfd657739cc099473b122a3dde855220a901d
dqnk <64268180+dqnk@users.noreply.github.com>
2024-05-20 11:08:02 +0200

    diffs

--- lua/molokai/init.lua
+++ lua/molokai/init.lua
@@ -63,6 +63,7 @@ function molokai.setup()
 		{ "@text.title", { fg = colors.green, bold = true } },
 		{ "@text.literal", { fg = colors.yellow, italic = true } },
 		{ "@type", { fg = colors.blue } },
+		{ "@lsy.type", { link = "@keyword" } },
 		{ "@type.definition", { fg = colors.orange } },
 		{ "@variable", { fg = colors.orange } },
 		{ "@variable.builtin", { fg = colors.orange } },
